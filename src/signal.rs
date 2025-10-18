721e42b75842e6211598f765f2fe7ccc3c6efe41
fosghen <r.efremov2@g.nsu.ru>
2025-10-18 14:59:29 +0700

    add read .wav and test for read/write .wav

--- src/signal.rs
+++ src/signal.rs
@@ -13,9 +13,43 @@ pub fn save_wave(signal: &Vec<f64>, filename: &str) -> Result<(), hound::Error> 
 
     let mut writer = hound::WavWriter::create(filename, spec).unwrap();
     for value in signal {
-        let amplitude = f32::MAX as f64;
-        writer.write_sample((value * amplitude) as f32)?;
+        // let amplitude = f32::MAX as f64;
+        writer.write_sample(*value as f32)?;
     }
     Ok(())
 }
 
+pub fn read_wave(signal: &mut Vec<f64>, filename: &str) -> Result<(), hound::Error> {
+    let mut reader = hound::WavReader::open(filename)?;
+    
+    for sample in reader.samples::<f32>(){
+        signal.push(sample? as f64);
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::generate;
+
+    use super::*;
+
+    #[test]
+    fn test_save_read_wave() {
+        let signal = generate::create_sine(133., 0., 0.1, 0.5);
+        let fname = "test_o.wav";
+
+        save_wave(&signal, fname).expect("failed to save wav");
+
+        let mut readed_signal: Vec<f64> = Vec::new();
+
+        read_wave(&mut readed_signal, fname).expect("failed to open wav");
+
+        assert!(signal.iter().zip(readed_signal.iter()).all(|(x, y)| (x - y).abs() < 1e-6), "Writed signal not equal readed signal");
+
+        fs::remove_file(fname).ok();
+    }
+}
